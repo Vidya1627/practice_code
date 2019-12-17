c40c350f2993144245df9df1a83efc424b1f3fc7
Vidhya Paliwal <vidhya.paliwal@gmail.com>
2019-12-17 20:42:38 +0530

    Rod-Cut problem DP solution
    It prints the maximum achievable by appropriate
    cuts, as well prints the optimal cut lengths.
    
    Author: Vidya Paliwal

---
+++ rod_cut.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int P[10] = {1, 5, 8, 9, 10, 17, 17, 20, 24, 10};
+int ans[11] = {0};
+
+int f(int n) {
+    if (n==0) {
+        return 0;
+    }
+    if (ans[n]!=0) {
+        return ans[n];
+    }
+    int max = P[n-1];
+    for (int i=1;i<n;i++) {
+        int val = P[i-1] + f(n-i);
+        if (val > max) {
+            max = val;
+        }
+    }
+
+    ans[n] = max;
+    return max;
+}
+
+void cut(int n){
+    if(n == 0){
+        return;
+    }
+    int max = -1, f_cut = 0;
+    for(int i = 1; i <= n; i++){
+        if(P[i-1]+ans[n-i] > max){
+            max = P[i-1]+ans[n-i];
+            f_cut = i;
+        }    
+    }
+    cout<<f_cut<<" ";
+    cut(n - f_cut);
+}
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<f(n)<<endl;
+	cut(n);
+	return 0;
+}
